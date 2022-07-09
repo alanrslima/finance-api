988b197f92c9c45b63621b4fa97cfa72bc6a804d
Alan Lima <alanronison.lima@gmail.com>
2022-07-09 13:20:31 -0300

    included roles

---
+++ ormconfig.js
@@ -0,0 +1,13 @@
+module.exports = {
+  type: "mysql",
+  port: process.env.DB_PORT,
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: "finance",
+  entities: ["./src/entities/*.ts"],
+  migrations: ["./src/database/migrations/*.ts"],
+  cli: {
+    migrationsDir: "./src/database/migrations",
+  },
+};
