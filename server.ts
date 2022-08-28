fee081a1f748e8e452dafc38eec6f0c1bb0da48b
Alan Lima <alanronison.lima@gmail.com>
2022-08-28 13:08:02 -0300

    feat: included docker configs

--- server.ts
+++ server.ts
@@ -1,9 +1,7 @@
 import app from "./src/app";
-
 /**
  * Get port from environment and store in Express.
  */
- const port = process.env.PORT ? process.env.PORT : 3000;
+const port = process.env.DOCKER_PORT || 3000;
 
- app.listen(port, () => console.log(`Server is running on port ${port}`));
- 
+app.listen(port, () => console.log(`Server is running on port ${port}`));
